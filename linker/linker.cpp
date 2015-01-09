9279827cd9b14ffb81a5ea4c2ffe857d243b48ae
Justin M <justinjams@gmail.com>
2015-01-09 00:00:58 -0500

    Revert "Allow non-PIE executables"
    This reverts commit e8b35f20404de9bbd7c640cacccf0e2d17d4cc26.

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -2179,8 +2179,8 @@ static ElfW(Addr) __linker_init_post_relocation(KernelArgumentBlock& args, ElfW(
 
     ElfW(Ehdr)* elf_hdr = reinterpret_cast<ElfW(Ehdr)*>(si->base);
     if (elf_hdr->e_type != ET_DYN) {
-        __libc_format_fd(2, "error: Not a PIE executable, but loading anyway.\n");
-        /*exit(EXIT_FAILURE);*/
+        __libc_format_fd(2, "error: only position independent executables (PIE) are supported.\n");
+        exit(EXIT_FAILURE);
     }
 
     // Use LD_LIBRARY_PATH and LD_PRELOAD (but only if we aren't setuid/setgid).
