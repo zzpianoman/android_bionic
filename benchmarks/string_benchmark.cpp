903e54dd2f15db3c2110f3797015eea9fcc477d0
Kyle Repinski <repinski23@gmail.com>
2015-04-30 05:26:08 -0500

    benchmarks: Add strchr benchmark.
    Not totally sure on this code, but it works.
    
    Change-Id: I421cc7b02f9a4ff15c82674abe50a2dc332c25ed
    
    Conflicts:
    	benchmarks/string_benchmark.cpp

--- benchmarks/string_benchmark.cpp
+++ benchmarks/string_benchmark.cpp
@@ -121,7 +121,8 @@ void BM_string_strlen::Run(int iters, int nbytes) {
   SetBenchmarkBytesProcessed(uint64_t(iters) * uint64_t(nbytes));
   delete[] s;
 }
-BENCHMARK(BM_string_strchr)->AT_COMMON_SIZES;
+
+
 
 static void BM_string_strrchr(int iters, int nbytes) {
   StopBenchmarkTiming();
@@ -227,3 +228,24 @@ static void BM_string_memrchr(int iters, int nbytes) {
   delete[] s;
 }
 BENCHMARK(BM_string_memrchr)->AT_COMMON_SIZES;
+
+static void BM_string_strchr(int iters, int nbytes) {
+  StopBenchmarkTiming();
+  char* s = new char[nbytes];
+  memset(s, 'x', nbytes);
+  s[nbytes - 1] = 0;
+  StartBenchmarkTiming();
+
+  volatile char* c = new char[1];
+  volatile int d __attribute__((unused)) = 0;
+  for (int i = 0; i < iters; ++i) {
+    c = strchr(s, 'y');
+    if (c != NULL)
+        d += (int)c[0];
+  }
+
+  StopBenchmarkTiming();
+  SetBenchmarkBytesProcessed(int64_t(iters) * int64_t(nbytes));
+  delete[] s;
+}
+BENCHMARK(BM_string_strchr)->AT_COMMON_SIZES;
